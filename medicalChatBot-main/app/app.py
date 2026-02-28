79e0001bc4efa7d9d77061b7b7d4dc8dc07fa724
Sachin kumar <stmsachinkumar9874@gmail.com>
2026-02-28 23:27:26 +0530

    root requirement file

--- medicalChatBot-main/app/app.py
+++ medicalChatBot-main/app/app.py
@@ -19,4 +19,6 @@ def ask():
     return jsonify(answer=response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000)
+   #this is change to this
+   # app.run(debug=True)
